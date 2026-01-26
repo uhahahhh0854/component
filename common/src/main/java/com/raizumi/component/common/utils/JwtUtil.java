1c4d5bb1c12a595ecd71d20e51aa450e94f6ae25
UHAhahhh <2873585250@qq.com>
2026-01-26 18:50:27 +0800

    Add password and token util.

---
+++ common/src/main/java/com/raizumi/component/common/utils/JwtUtil.java
@@ -0,0 +1,37 @@
+package com.raizumi.component.common.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+public class JwtUtil {
+
+    public static Key key(String secretKey) {
+        return Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public static String generateJWT(Key key, String subject, long ttlMillis) {
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+
+        return Jwts.builder()
+                .setSubject(subject)
+                .setIssuedAt(now)
+                .setExpiration(new Date(nowMillis + ttlMillis))
+                .signWith(key)
+                .compact();
+    }
+
+    public static String parseJWT(Key key, String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+}
